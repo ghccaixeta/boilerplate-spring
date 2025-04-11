7467efe05987fce8e5eb84b1cdd9001ab23be687
Gustavo Caixeta <gustavohcaixeta@gmail.com>
2025-04-11 10:19:30 -0300

    feat: adiciona configuração de autenticação e autorização com JWT, incluindo entidades, repositórios e controladores para gerenciamento de usuários e roles

--- src/main/java/com/example/demo/config/AuthorizationServerConfig.java
+++ src/main/java/com/example/demo/config/AuthorizationServerConfig.java
@@ -1,7 +1,10 @@
 package com.example.demo.config;
 
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,10 +12,13 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
@@ -22,25 +28,20 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import com.example.demo.entities.OAuthClient;
 import com.example.demo.repository.OauthClientRepository;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 
 @Configuration
 @EnableWebSecurity
 public class AuthorizationServerConfig {
 
-  @Bean
-  PasswordEncoder passwordEncoder() {
-    return new PasswordEncoder() {
-      @Override
-      public String encode(CharSequence rawPassword) {
-        return rawPassword.toString();
-      }
+  @Value("${jwt.public.key}")
+  private RSAPublicKey publicKey;
 
-      @Override
-      public boolean matches(CharSequence rawPassword, String encodedPassword) {
-        return rawPassword.toString().equals(encodedPassword);
-      }
-    };
-  }
+  @Value("${jwt.private.key}")
+  private RSAPrivateKey privateKey;
 
   @Bean
   SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -101,4 +102,24 @@ public class AuthorizationServerConfig {
       context.getClaims().claim("sub", "");
     };
   }
+
+  @Bean
+  public JwtEncoder jwtEncoder() {
+    JWK jwk = new RSAKey.Builder(this.publicKey).privateKey(this.privateKey).build();
+
+    var jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+
+    return new NimbusJwtEncoder(jwks);
+  }
+
+  @Bean
+  public JwtDecoder jwtDecoder() {
+    return NimbusJwtDecoder.withPublicKey(publicKey).build();
+  }
+
+  @Bean
+  public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
 }
